bed5e7d427f6a4d9659be1034d964963ad27c99f
Sav Younts <savyounts@gmail.com>
2018-08-08 15:40:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,7 +23,10 @@ class Song
   end 
   
   def self.create_by_name(name)
-    self.new_by_name(name).save
+    song = self.new 
+    song.name = name
+    song.save
+    song
   end 
   
   def self.find_by_name(name)
